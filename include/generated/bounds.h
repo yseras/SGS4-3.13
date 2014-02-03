7fb00044ebb8766eaebbbd657a84a2222117ad76
ysera <kiwiis.zero@gmail.com>
2014-02-03 01:22:48 -0500

    Add in SDHCI-MSM support (original authorship: https://lkml.org/lkml/2014/1/30/352)

--- include/generated/bounds.h
+++ include/generated/bounds.h
@@ -10,7 +10,6 @@
 #define NR_PAGEFLAGS 22 /* __NR_PAGEFLAGS	@ */
 #define MAX_NR_ZONES 3 /* __MAX_NR_ZONES	@ */
 #define NR_PCG_FLAGS 3 /* __NR_PCG_FLAGS	@ */
-#define NR_CPUS_BITS 2 /* ilog2(CONFIG_NR_CPUS)	@ */
-#define SPINLOCK_SIZE 4 /* sizeof(spinlock_t)	@ */
+#define SPINLOCK_SIZE 0 /* sizeof(spinlock_t)	@ */
 
 #endif
