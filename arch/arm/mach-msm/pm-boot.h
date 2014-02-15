fc76061af0972e68fbbbf199e80bf7d53e45cfba
ysera <kiwiis.zero@gmail.com>
2014-02-15 03:10:50 -0500

    pm-boot.h: Readded structs/enums depended on by board-jf_tmo

--- arch/arm/mach-msm/pm-boot.h
+++ arch/arm/mach-msm/pm-boot.h
@@ -13,6 +13,21 @@
 #ifndef _ARCH_ARM_MACH_MSM_PM_BOOT_H
 #define _ARCH_ARM_MACH_MSM_PM_BOOT_H
 
+/* Added for backwards compatibility */
+enum {
+	MSM_PM_BOOT_CONFIG_TZ		     = 0,
+	MSM_PM_BOOT_CONFIG_RESET_VECTOR_PHYS = 1,
+	MSM_PM_BOOT_CONFIG_RESET_VECTOR_VIRT = 2,
+	MSM_PM_BOOT_CONFIG_REMAP_BOOT_ADDR   = 3,
+};
+
+struct msm_pm_boot_platform_data {
+	int mode;
+	phys_addr_t  p_addr;
+	void __iomem *v_addr;
+};
+/* ********************************* */
+
 void msm_pm_boot_config_before_pc(unsigned int cpu, unsigned long entry);
 void msm_pm_boot_config_after_pc(unsigned int cpu);
 
