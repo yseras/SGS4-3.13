6438e31280599ea0499af0a0cb4a705406a1d127
ysera <kiwiis.zero@gmail.com>
2014-02-15 03:31:02 -0500

    Board-jf_tmo: Rename freq_step to bootup_freq_step
    msm_thermal_data struct (as defined in include/linux/msm_thermal.h) had
    changed significantly and freq_step was renamed, so update the board
    file to reflect this.
    
    There may also be some other useful things added in msm_thermal.h which
    I may wish to explore adding into our board files down the line.

--- arch/arm/mach-msm/board-jf_tmo.c
+++ arch/arm/mach-msm/board-jf_tmo.c
@@ -3098,7 +3098,7 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.poll_ms = 250,
 	.limit_temp_degC = 60,
 	.temp_hysteresis_degC = 10,
-	.freq_step = 2,
+	.bootup_freq_step = 2,
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
