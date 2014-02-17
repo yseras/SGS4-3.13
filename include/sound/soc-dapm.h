a1a43819e63b656fec0f4438cc68ce52ac267c8e
ysera <kiwiis.zero@gmail.com>
2014-02-17 14:53:46 -0500

    WCD9310: Properly fix SND_SOC_DAPM_MICBIAS_E
    Left out some parameters and initialized things twice. Now it should be
    fine.

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -159,8 +159,9 @@ struct device;
 	.kcontrol_news = wcontrols, \
 	.num_kcontrols = wncontrols, .event = wevent, .event_flags = wflags}
 #define SND_SOC_DAPM_MICBIAS_E(wname, wreg, wshift, winvert, wevent, wflags) \
-{	.id = snd_soc_dapm_micbias, .name = wname, .reg = wreg, .shift = wshift, \
+{	.id = snd_soc_dapm_micbias, .name = wname,  \
 	SND_SOC_DAPM_INIT_REG_VAL(wreg, wshift, winvert), \
+	.kcontrol_news = NULL, .num_kcontrols = 0, \
 	.event = wevent, .event_flags = wflags}
 #define SND_SOC_DAPM_SWITCH_E(wname, wreg, wshift, winvert, wcontrols, \
 	wevent, wflags) \
