e9dbca68d0c954577392889a3523971022b89bb9
ysera <kiwiis.zero@gmail.com>
2014-02-18 05:37:17 -0500

    TEST: eS325: Fix 'noinline_for_stack' function
    vfs_getattr has changed since the kernel versions this source was based
    upon. Fix the 'noinline_for_stack' function that calls it.

--- sound/soc/codecs/es325.c
+++ sound/soc/codecs/es325.c
@@ -1978,10 +1978,10 @@ static const char *fw_path[] = {
 };
 
 /* Don't inline this: 'struct kstat' is biggish */
-static noinline_for_stack long fw_file_size(struct file *file)
+static noinline_for_stack long fw_file_size(struct path path)
 {
 	struct kstat st;
-	if (vfs_getattr(file->f_path.mnt, file->f_path.dentry, &st))
+	if (vfs_getattr(&path, &st))
 		return -1;
 	if (!S_ISREG(st.mode))
 		return -1;
