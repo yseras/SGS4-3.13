7bc3bc0d77ad6c805de6b95738562ed70164c7b6
yseras <kiwiis.zero@gmail.com>
2014-03-03 06:43:24 -0500

    drivers/clk: export of_clk_add_provider()
    This function is called by a couple of files and they need it exported.
    Simply compiling clk.c itself throws up a whole lot of redefinition
    errors, so we need to "cherry-pick" of_clk_add_provider without getting
    the rest of the functions from clk.c exported by using #ifdefs as
    MSM-3.10 does.
    
    Because I try to keep my changes to mainline files as minimal as
    possible, instead of changing drivers/clk/Makefile to compile clk.c with
    CONFIG_OF as MSM-3.10 has done, I'm compiling it with mach-msm's
    Makefile instead.

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -21,6 +21,8 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 
+#if defined(CONFIG_COMMON_CLK)
+
 static DEFINE_SPINLOCK(enable_lock);
 static DEFINE_MUTEX(prepare_lock);
 
@@ -2087,6 +2089,8 @@ int clk_notifier_unregister(struct clk *clk, struct notifier_block *nb)
 }
 EXPORT_SYMBOL_GPL(clk_notifier_unregister);
 
+#endif /* CONFIG_COMMON_CLK */
+
 #ifdef CONFIG_OF
 /**
  * struct of_clk_provider - Clock provider registration structure
@@ -2119,6 +2123,8 @@ struct clk *of_clk_src_simple_get(struct of_phandle_args *clkspec,
 }
 EXPORT_SYMBOL_GPL(of_clk_src_simple_get);
 
+#if defined(CONFIG_COMMON_CLK)
+
 struct clk *of_clk_src_onecell_get(struct of_phandle_args *clkspec, void *data)
 {
 	struct clk_onecell_data *clk_data = data;
@@ -2132,6 +2138,8 @@ struct clk *of_clk_src_onecell_get(struct of_phandle_args *clkspec, void *data)
 	return clk_data->clks[idx];
 }
 EXPORT_SYMBOL_GPL(of_clk_src_onecell_get);
+
+#endif /* CONFIG_COMMON_CLK */
 
 /**
  * of_clk_add_provider() - Register a clock provider for a node
@@ -2232,6 +2240,8 @@ const char *of_clk_get_parent_name(struct device_node *np, int index)
 }
 EXPORT_SYMBOL_GPL(of_clk_get_parent_name);
 
+#if defined(CONFIG_COMMON_CLK)
+
 /**
  * of_clk_init() - Scan and init clock providers from the DT
  * @matches: array of compatible values and init functions for providers.
@@ -2252,4 +2262,7 @@ void __init of_clk_init(const struct of_device_id *matches)
 		clk_init_cb(np);
 	}
 }
+
+#endif /* CONFIG_COMMON_CLK */
+
 #endif
